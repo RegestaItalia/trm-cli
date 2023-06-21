84c97279d9154e9cf1945a6a96840a9109327bec
simonegaffurini <simone.gaffurini@regestaitalia.it>
2023-06-21 17:40:30 +0200

    Sktd constant

--- src/manifest/sktd/wrapper/xmlGenerator/writeContent.js
+++ src/manifest/sktd/wrapper/xmlGenerator/writeContent.js
@@ -1,7 +1,9 @@
 module.exports = (args) => {
-    const content = Buffer.from(args.content).toString('base64');
-    const timestamp = new Date().toISOString();
-    return `<?xml version="1.0" encoding="UTF-8"?>
+  var oContent = args.content || {};
+  oContent.isTrmManifest = true;
+  const content = Buffer.from(oContent).toString('base64');
+  const timestamp = new Date().toISOString();
+  return `<?xml version="1.0" encoding="UTF-8"?>
     <sktd:docu xmlns:adtcore="http://www.sap.com/adt/core" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sktd="http://www.sap.com/wbobj/texts/sktd" adtcore:changedAt="${timestamp}" adtcore:changedBy="${args.user}" adtcore:createdAt="${args.tdevc.createdOn.toISOString()}" adtcore:createdBy="${args.tdevc.as4user}" adtcore:description="${args.tdevc.ctext}" adtcore:language="${args.langu || 'EN'}" adtcore:name="${args.objName}" adtcore:type="SKTD/TYP" adtcore:version="inactive" adtcore:abapLanguageVersion="0" adtcore:masterLanguage="${args.langu || 'EN'}" adtcore:masterSystem="${args.sid}" adtcore:responsible="${args.user}">
       <atom:link href="versions" rel="http://www.sap.com/adt/relations/versions" title="Historic versions"/>
       <atom:link href="/sap/bc/adt/repository/informationsystem/abaplanguageversions?uri=%2Fsap%2Fbc%2Fadt%2Fdocumentation%2Fktd%2Fdocuments%2F${args.objName.trim().toLowerCase()}" rel="http://www.sap.com/adt/relations/informationsystem/abaplanguageversions" title="Allowed ABAP language versions" type="application/vnd.sap.adt.nameditems.v1+xml"/>
