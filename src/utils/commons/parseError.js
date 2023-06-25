0a2a16959ed07bf2e9ae43f9ce58d4a7b2a0cf6e
simonegaffurini <simone.gaffurini@regestaitalia.it>
2023-06-25 16:52:57 +0200

    Beautify parseError

--- src/utils/commons/parseError.js
+++ src/utils/commons/parseError.js
@@ -1,32 +1,32 @@
 const { parseAbapError } = require('../../functions/utils');
 
 module.exports = (e) => {
-    if (e.name === 'ABAPError') {
-        return parseAbapError(e);
-    } else if (e.name === 'RfcLibError') {
-        debugger
-    } else if (e.name === 'AxiosError') {
-        if(e.response.status === 401){
-            return `TRM Registry error: Missing login data`;
-        }else{
-            try {
+	if (e.name === 'ABAPError') {
+		return parseAbapError(e);
+	} else if (e.name === 'RfcLibError') {
+		debugger
+	} else if (e.name === 'AxiosError') {
+		if (e.response.status === 401) {
+			return `TRM Registry error: Missing login data`;
+		} else {
+			try {
 				const headers = e.response.headers;
 				if (headers['x-trm-registry'] === 'public') {
 					var message;
-					try{
+					try {
 						message = e.response.data.message;
-					}catch(e){
+					} catch (e) {
 						message = e.response.statusText;
 					}
 					return `TRM Registry error: ${message}`;
-				}else{
+				} else {
 					return e.toString();
 				}
 			} catch (err) {
 				return err.toString();
 			}
-        }
-    } else {
-        return e.toString();
-    }
+		}
+	} else {
+		return e.toString();
+	}
 }
